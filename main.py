b92051a25c52962f08e8933056b60511e59bfc4c
Faithirakoze <f.irakoze2@alustudent.com>
2025-05-19 18:02:11 -0700

    Complete Program

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import re
 
-#Regex capturing correct email formats
+# Regex capturing correct email formats
 email = '''
 Please contact me at faith@gmail.com for more information.
 You can also reach me at f.irakoze2@@company.co.uk or faith-irakoze@alu.edu.'''
@@ -10,7 +10,7 @@ email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 email_matches = re.findall(email_pattern, email)
 print(f"Emails:{email_matches}")
 
-#Regular expression extracting valid URLs
+# Regular expression extracting valid URLs
 urls = '''
 You can also check:
 - http://example.org/about-me
@@ -21,7 +21,7 @@ urls_pattern = r'https?:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\/\S*)?'
 url_matches = re.findall(urls_pattern, urls)
 print(f"URLs: {url_matches}")
 
-#Regular expression capturing valid phone numbers
+# Regular expression capturing valid phone numbers
 phone_number = '''
 For support, call us me:
 - (123) 456-7890
@@ -32,7 +32,7 @@ phone_pattern = r'(?:\(?\d{3}\)?)[\s\-\.]\d{3}[\s\-\.]\d{4}'
 phone_matches = re.findall(phone_pattern, phone_number)
 print(f"Phone numbers: {phone_matches}")
 
-#Regular expression capturing valid credit card numbers
+# Regular expression capturing valid credit card numbers
 credit_card = '''
 Here are some of my credit card numbers:
 1234 5678 9012 3456
@@ -42,7 +42,7 @@ creditcard_pattern = r'[0-9]{4}[\s\-][0-9]{4}[\s\-][0-9]{4}[\s\-][0-9]{4}'
 creditcard_matches = re.findall(creditcard_pattern, credit_card)
 print(f"Credit card numbers: {creditcard_matches}")
 
-#Regular expression capturing valid time formats
+# Regular expression capturing valid time formats
 time = '''
 I usually go to school at 9:30 AM and have lunch break at 12:45 PM.
 I leave school at 13:75 or 19:45 and sleep at 23:59 or 24:01'''
